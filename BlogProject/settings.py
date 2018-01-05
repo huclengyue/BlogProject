95eaa447cede480141df9ed1f8a65ded225f175c
LengYue <53542468awdz>
2018-01-05 09:52:04 +0800

    add attach

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kxd!q)+a)iq-$!exc7olc896^9vru6q@x7w(nfmzgt1jf79yp@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.apkdv.com']
 
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 部署加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 HAYSTACK_CONNECTIONS = {
     'default': {
